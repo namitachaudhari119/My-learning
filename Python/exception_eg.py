5c504503f334fc53ffb8339583c0c2f3cd7f8b23
namita_chaudhari <namita.chaudhari@sungardas.com>
2016-01-06 11:07:28 +0530

    Exception handling

--- Python/exception_eg.py
+++ Python/exception_eg.py
@@ -1,4 +1,4 @@
-#Program with no problem
+#Program with no exception
 try:
    fh = open("testfile", "w")
    fh.write("This is my test file for exception handling!!")
@@ -27,3 +27,26 @@ try:
       fh.close()
 except IOError:
    print "Error: can\'t find file or read data"
+   
+#Program with argument exception
+
+# Define a function here.
+def temp_convert(var):
+   try:
+      return int(var)
+   except ValueError, Argument:
+      print "The argument does not contain numbers\n", Argument
+
+# Call above function here.
+temp_convert("xyz");
+
+#User defined exceptions
+class Networkerror(RuntimeError):
+   def __init__(self, arg):
+      self.args = arg
+
+def demo():
+try:
+   raise Networkerror("Bad hostname")
+except Networkerror,e:
+   print e.args
