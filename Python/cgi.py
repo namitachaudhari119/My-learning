f937d998c566775fb17801032c925404ac38f4f8
namita_chaudhari <namita.chaudhari@sungardas.com>
2016-01-19 10:07:44 +0530

    CGI

--- Python/cgi.py
+++ Python/cgi.py
@@ -19,3 +19,11 @@ print "<body>"
 print "<h2>Hello %s %s</h2>" % (first_name, last_name)
 print "</body>"
 print "</html>"
+
+# ....................... Setting cookie
+print "Set-Cookie:UserID=XYZ;\r\n"
+print "Set-Cookie:Password=XYZ123;\r\n"
+print "Set-Cookie:Expires=Tuesday, 31-Dec-2007 23:12:40 GMT";\r\n"
+print "Set-Cookie:Domain=www.tutorialspoint.com;\r\n"
+print "Set-Cookie:Path=/perl;\n"
+print "Content-type:text/html\r\n\r\n"
